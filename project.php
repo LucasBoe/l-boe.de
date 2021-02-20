8aec7c5da44b2080bfb38b37b61a2af495631ae2
LucasBoe <lucas@lumotex.de>
2021-02-20 11:43:14 +0100

    fixed link from project page back to main page

--- project.php
+++ project.php
@@ -64,7 +64,7 @@ $conn->close();
     <?php echo $content; ?>
     </div>
   </div>
-  <div id="close" class="icon"><a href="index.html"><<span class="label">Back</span></a></div>
+  <div id="close" class="icon"><a href="https://l-boe.de"><<span class="label">Back</span></a></div>
 </body>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
